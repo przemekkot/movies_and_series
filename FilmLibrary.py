1bd4cbc65c3e22cdee99975d5f4c842337693cf4
przemekkot <65823229+przemekkot@users.noreply.github.com>
2020-06-23 22:13:37 +0200

    Correct functions
    Add more films and series to the base
    Correct codes for functions in points 7 - 11 in the set

--- FilmLibrary.py
+++ FilmLibrary.py
@@ -35,10 +35,14 @@ class Series(Film):
 film1 = Film(title="Forrest Gump", year="1994", genre="comedy", times_played=0)
 film2 = Film(title="Twister", year="1996", genre="catastrophic", times_played=0)
 film3 = Film(title="Sinister", year="2012", genre="horror", times_played=0)
+film4 = Film(title="Argo", year="2012", genre="thriller", times_played=0)
+film5 = Film(title="Focus", year="2015", genre="comedy", times_played=0)
 
 series1_1 = Series(title="Friends", year="1994", genre="comedy", season="01", episode="01", times_played=0)
 series1_2 = Series(title="Friends", year="1994", genre="comedy", season="01", episode="02", times_played=0)
 series1_3 = Series(title="Friends", year="1994", genre="comedy", season="01", episode="03", times_played=0)
+series1_4 = Series(title="Friends", year="1994", genre="comedy", season="01", episode="04", times_played=0)
+series1_5 = Series(title="Friends", year="1994", genre="comedy", season="01", episode="05", times_played=0)
 
 #print(film1)
 #print(film2)
@@ -48,7 +52,6 @@ series1_3 = Series(title="Friends", year="1994", genre="comedy", season="01", ep
 #print(series1_3)
 #print(series1_3)
 
-#load_to_list = ""
 full_list = []
 films = []
 series = []
@@ -56,9 +59,13 @@ series = []
 full_list.append(film1)
 full_list.append(film2)
 full_list.append(film3)
+full_list.append(film4)
+full_list.append(film5)
 full_list.append(series1_1)
 full_list.append(series1_2)
 full_list.append(series1_3)
+full_list.append(series1_4)
+full_list.append(series1_5)
 
 #print(full_list)
 #sorted_full_list = sorted(full_list, key=operator.attrgetter('title'))
@@ -67,12 +74,12 @@ full_list.append(series1_3)
 #full_list_sorted = sorted(full_list, key=lambda 1: 1.count, reverse=True)
 #print(full_list_sorted)
 
+#7
 def get_movies():
       for i in full_list:
-        films = [i for i in full_list if isinstance(i, Film)]
+        films = [i for i in full_list if type(i) == Film]
         sorted_films = sorted(films, key=operator.attrgetter('title'))
-      else:
-        pass
+        #sorted_films = sorted(films, key=lambda Film: Film.title)
       return sorted_films
       
 print(get_movies())
@@ -81,44 +88,71 @@ def get_series():
       for i in full_list:
         series = [i for i in full_list if isinstance(i, Series)]
         sorted_series = sorted(series, key=operator.attrgetter('title'))
-      else:
-        pass
+        #sorted_series = sorted(series, key=lambda Series: Series.title)
       return sorted_series
 
 print(get_series())
 
-
-search_result = []
+#8
 
 def search(name):
     for i in full_list:
-        search_result = [i for i in full_list if name == operator.attrgetter('title')]
+        if name in i.title:
+        #if i.title == name:
+            return i
     else:
         print("W bazie nie ma takiego tytułu")
-    return search_result
 
 print(search('Twister'))
 
-
-random_result = []
+#9
 
 def generate_views():
-    for i in full_list:
-        random_result = [random.choice(full_list)]
-    #operator.attrgetter('times_played') = random.randint(1,100)
+    random_result = random.choice(full_list)
+    #random_result.times_played = random.randint(1,100)
+    for i in range(random.randint(1,100)):
+        random_result.play()
     return random_result
 
 print(generate_views())
 
+#10
 
-random_10_results = []
 result = 0
+random_10_results = []
 
 def generate_views_10times():
-    for i in range(10):
-        for i in full_list:
-            result = random.choice(full_list)
-            random_10_results.append(result)
+    for n in range(10):
+        result = generate_views()
+        random_10_results.append(result)
     return random_10_results
 
 print(generate_views_10times())
+
+#11
+
+def top_titles(top):
+    top_n_titles = []
+    top_n_titles = sorted(random_10_results, key = lambda Film: Film.times_played, reverse = True)[:top]
+    return top_n_titles
+
+print(top_titles(3))
+
+#top_titles() z content_type
+
+content_type = ("top_films", "top_series")
+
+def top_titles(content_type, top):
+    top_n_type_titles = []
+    if content_type == "top_films":
+        for i in random_10_results:
+            if type(i) == Film:
+                top_n_type_titles = sorted(random_10_results, key = lambda i: i.times_played, reverse = True)[:top]
+    elif content_type == "top_series":
+       top_n_type_titles = sorted(random_10_results, key = lambda Series: Series.times_played, reverse = True)[:top]
+    else:
+        top_n_type_titles = sorted(random_10_results, key = lambda Film: Film.times_played, reverse = True)[:top]
+    return top_n_type_titles
+
+print(top_titles("top_films", 2))
+
